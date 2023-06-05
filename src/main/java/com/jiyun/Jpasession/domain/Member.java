b070825734b8c2c72288f7a3c98c7745687812e0
june0216 <ziyun1612@ewhain.net>
2023-06-05 15:06:34 +0900

    :sparkles: 4장 엔티티 - 회원 조건에 따라 필드 추가

--- src/main/java/com/jiyun/Jpasession/domain/Member.java
+++ src/main/java/com/jiyun/Jpasession/domain/Member.java
@@ -3,10 +3,8 @@ package com.jiyun.Jpasession.domain;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @NoArgsConstructor //해당 클래스에 기본 생성자를 만들줌 -> @Entity가 그 기능을 하지만 붙이는 이유는 기본생성자를 추가하되, 접근제한을 걸어서 안전성도 높이는 것이다.
 @Table(name = "member")
@@ -18,6 +16,20 @@ public class Member {
 
     @Column(name = "NAME")
     private String username;
+
+    @Enumerated(EnumType.STRING) // 회원은 일반 회원과 관리자로 구분해야 하므로 자바의 enum사용
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob //길이 제한이 없기 때문에
+    private String description;
+
+
 
     //매핑 정보가 없는 필드
     private Integer age;
